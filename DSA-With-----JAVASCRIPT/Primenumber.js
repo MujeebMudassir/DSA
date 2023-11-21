a60be26eec87f5059712785a31b6e1fb9867d2cf
MujeebMudassir <mujeebmujeeb2071@gmail.com>
2023-11-21 22:22:32 +0530

    staged

--- DSA-With-----JAVASCRIPT/Primenumber.js
+++ DSA-With-----JAVASCRIPT/Primenumber.js
@@ -3,14 +3,18 @@ function isPrime(n) {
     console.log("a");
     return false;
   }
-  if (n % 2 === 0) {
-    console.log("b");
-    return false;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      console.log("b");
+      return false;
+    }
   }
   console.log("c");
   return true;
 }
 
 console.log(isPrime(9));
+console.log(isPrime(2));
 console.log(isPrime(73));
 console.log(isPrime(89));
